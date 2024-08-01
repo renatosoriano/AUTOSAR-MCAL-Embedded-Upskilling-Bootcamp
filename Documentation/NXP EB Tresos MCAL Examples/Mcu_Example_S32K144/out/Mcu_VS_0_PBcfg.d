037e63a85439f2d5374ab645663d43b517a984eb
Renato <renatosd_19@hotmail.com>
2024-07-31 22:50:06 -0700

    Added NXP Mcu_Example_S32K144

---
+++ Documentation/NXP EB Tresos MCAL Examples/Mcu_Example_S32K144/out/Mcu_VS_0_PBcfg.d
@@ -0,0 +1,92 @@
+Mcu_VS_0_PBcfg.o: generate/src/Mcu_VS_0_PBcfg.c \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mcu_TS_T40D2M20I0R0/include/Mcu.h \
+ generate/include/Mcu_Cfg.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Mcal.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Compiler.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Compiler_Cfg.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/CompilerDefinition.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/StandardTypes.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Platform_Types.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/PlatformTypes.h \
+ generate/include/Soc_Ips.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Platform_Types.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/IpVersionMacros.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Reg_eSys.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/OsIf_Internal.h \
+ generate/include/OsIf_Cfg.h generate/include/OsIf_ArchCfg.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/StandardTypes.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/header/S32K144_SYSTICK.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/header/S32K144_COMMON.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/BasicTypes.h \
+ generate/include/Clock_Ip_Cfg.h generate/include/Clock_Ip_VS_0_PBcfg.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mcu_TS_T40D2M20I0R0/include/Clock_Ip_Types.h \
+ generate/include/Clock_Ip_Cfg_Defines.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/header/S32K144_SIM.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/header/S32K144_SCG.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/header/S32K144_PCC.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/header/S32K144_SMC.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Mcu_MemMap.h \
+ generate/include/Power_Ip_Cfg.h generate/include/Power_Ip_VS_0_PBcfg.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mcu_TS_T40D2M20I0R0/include/Power_Ip_Types.h \
+ generate/include/Power_Ip_Cfg_Defines.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mcu_TS_T40D2M20I0R0/include/Power_Ip_SMC_Types.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mcu_TS_T40D2M20I0R0/include/Power_Ip_RCM_Types.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/header/S32K144_RCM.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mcu_TS_T40D2M20I0R0/include/Power_Ip_PMC_Types.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/header/S32K144_PMC.h \
+ generate/include/Ram_Ip_Cfg.h generate/include/Ram_Ip_VS_0_PBcfg.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mcu_TS_T40D2M20I0R0/include/Ram_Ip_Types.h \
+ generate/include/Ram_Ip_Cfg_Defines.h generate/include/Mcu_VS_0_PBcfg.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mcu_TS_T40D2M20I0R0/include/Mcu_EnvCfg.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mcu_TS_T40D2M20I0R0/include/Mcu_Ipw_Types.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mcu_TS_T40D2M20I0R0/include/Clock_Ip_Types.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mcu_TS_T40D2M20I0R0/include/Power_Ip_Types.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mcu_TS_T40D2M20I0R0/include/Ram_Ip_Types.h
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mcu_TS_T40D2M20I0R0/include/Mcu.h:
+generate/include/Mcu_Cfg.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Mcal.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Compiler.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Compiler_Cfg.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/CompilerDefinition.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/StandardTypes.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Platform_Types.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/PlatformTypes.h:
+generate/include/Soc_Ips.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Platform_Types.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/IpVersionMacros.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Reg_eSys.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/OsIf_Internal.h:
+generate/include/OsIf_Cfg.h:
+generate/include/OsIf_ArchCfg.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/StandardTypes.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/header/S32K144_SYSTICK.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/header/S32K144_COMMON.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/BasicTypes.h:
+generate/include/Clock_Ip_Cfg.h:
+generate/include/Clock_Ip_VS_0_PBcfg.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mcu_TS_T40D2M20I0R0/include/Clock_Ip_Types.h:
+generate/include/Clock_Ip_Cfg_Defines.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/header/S32K144_SIM.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/header/S32K144_SCG.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/header/S32K144_PCC.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/header/S32K144_SMC.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Mcu_MemMap.h:
+generate/include/Power_Ip_Cfg.h:
+generate/include/Power_Ip_VS_0_PBcfg.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mcu_TS_T40D2M20I0R0/include/Power_Ip_Types.h:
+generate/include/Power_Ip_Cfg_Defines.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mcu_TS_T40D2M20I0R0/include/Power_Ip_SMC_Types.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mcu_TS_T40D2M20I0R0/include/Power_Ip_RCM_Types.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/header/S32K144_RCM.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mcu_TS_T40D2M20I0R0/include/Power_Ip_PMC_Types.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/header/S32K144_PMC.h:
+generate/include/Ram_Ip_Cfg.h:
+generate/include/Ram_Ip_VS_0_PBcfg.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mcu_TS_T40D2M20I0R0/include/Ram_Ip_Types.h:
+generate/include/Ram_Ip_Cfg_Defines.h:
+generate/include/Mcu_VS_0_PBcfg.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mcu_TS_T40D2M20I0R0/include/Mcu_EnvCfg.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mcu_TS_T40D2M20I0R0/include/Mcu_Ipw_Types.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mcu_TS_T40D2M20I0R0/include/Clock_Ip_Types.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mcu_TS_T40D2M20I0R0/include/Power_Ip_Types.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mcu_TS_T40D2M20I0R0/include/Ram_Ip_Types.h:
