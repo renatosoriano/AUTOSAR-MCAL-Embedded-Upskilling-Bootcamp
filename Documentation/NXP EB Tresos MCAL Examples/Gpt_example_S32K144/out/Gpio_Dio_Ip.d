ed23b1727ce83af192967aa3b33e180e36c9f216
Renato <renatosd_19@hotmail.com>
2024-07-31 22:49:36 -0700

    Added NXP Gpt_example_S32K144

---
+++ Documentation/NXP EB Tresos MCAL Examples/Gpt_example_S32K144/out/Gpio_Dio_Ip.d
@@ -0,0 +1,32 @@
+Gpio_Dio_Ip.o: \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Dio_TS_T40D2M20I0R0/src/Gpio_Dio_Ip.c \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Dio_TS_T40D2M20I0R0/include/Gpio_Dio_Ip.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Std_Types.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/StandardTypes.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Platform_Types.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/PlatformTypes.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Compiler.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Compiler_Cfg.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/CompilerDefinition.h \
+ generate/include/Gpio_Dio_Ip_Cfg.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/header/S32K144_GPIO.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/header/S32K144_COMMON.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/BasicTypes.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Dio_MemMap.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Rte_TS_T40D2M20I0R0/include/SchM_Dio.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Rte_MemMap.h
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Dio_TS_T40D2M20I0R0/include/Gpio_Dio_Ip.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Std_Types.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/StandardTypes.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Platform_Types.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/PlatformTypes.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Compiler.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Compiler_Cfg.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/CompilerDefinition.h:
+generate/include/Gpio_Dio_Ip_Cfg.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/header/S32K144_GPIO.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/header/S32K144_COMMON.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/BasicTypes.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Dio_MemMap.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Rte_TS_T40D2M20I0R0/include/SchM_Dio.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Rte_MemMap.h:
---
+++ Documentation/NXP EB Tresos MCAL Examples/Gpt_example_S32K144/out/Gpt_Cfg.d
@@ -0,0 +1,18 @@
+Gpt_Cfg.o: generate/src/Gpt_Cfg.c generate/include/Gpt_Cfg.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Std_Types.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/StandardTypes.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Platform_Types.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/PlatformTypes.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Compiler.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Compiler_Cfg.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/CompilerDefinition.h \
+ generate/include/Gpt_VS_0_PBcfg.h
+generate/include/Gpt_Cfg.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Std_Types.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/StandardTypes.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Platform_Types.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/PlatformTypes.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Compiler.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Compiler_Cfg.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/CompilerDefinition.h:
+generate/include/Gpt_VS_0_PBcfg.h:
