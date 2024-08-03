f28e22c64b80e9df91b74e77d5826e4da016b7f7
Renato <renatosd_19@hotmail.com>
2024-08-03 01:16:09 -0700

    Examples updated

--- Documentation/NXP EB Tresos MCAL Examples/Adc_example_S32K144/out/Vector_Table.d
+++
@@ -1,2 +0,0 @@
-Vector_Table.o: \
- C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Platform_TS_T40D2M20I0R0/startup/src/m4/gcc/Vector_Table.s
--- Documentation/NXP EB Tresos MCAL Examples/Mem_Eep_Example_S32K144/out/startup_cm4.d
+++
@@ -1,2 +0,0 @@
-startup_cm4.o: \
- C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Platform_TS_T40D2M20I0R0/startup/src/m4/gcc/startup_cm4.s
