c53100481c526423cb1139410f3f4d5b4b8ce07c
Renato <renatosd_19@hotmail.com>
2024-08-03 01:20:43 -0700

    Added MemAcc_Example_S32K144

---
+++ Documentation/NXP EB Tresos S32K144 MCAL drivers Project Examples/MemAcc_Example_S32K144/generate/src/Trgmux_Ip_PBcfg.c
@@ -0,0 +1,115 @@
+/*==================================================================================================
+*   Project              : RTD AUTOSAR 4.7
+*   Platform             : CORTEXM
+*   Peripheral           : DMA,CACHE,TRGMUX,FLEXIO
+*   Dependencies         : none
+*
+*   Autosar Version      : 4.7.0
+*   Autosar Revision     : ASR_REL_4_7_REV_0000
+*   Autosar Conf.Variant :
+*   SW Version           : 2.0.0
+*   Build Version        : S32K1_RTD_2_0_0_P04_D2404_ASR_REL_4_7_REV_0000_20240417
+*
+*   Copyright 2020-2024 NXP Semiconductors
+*   
+*
+*   NXP Confidential. This software is owned or controlled by NXP and may only be
+*   used strictly in accordance with the applicable license terms. By expressly
+*   accepting such terms or by downloading, installing, activating and/or otherwise
+*   using the software, you are agreeing that you have read, and that you agree to
+*   comply with and are bound by, such license terms. If you do not agree to be
+*   bound by the applicable license terms, then you may not retain, install,
+*   activate or otherwise use the software.
+==================================================================================================*/
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/*==================================================================================================
+*                                        INCLUDE FILES
+* 1) system and project includes
+* 2) needed interfaces from external units
+* 3) internal and external interfaces from this unit
+==================================================================================================*/
+#include "Trgmux_Ip_Cfg.h"
+
+/*==================================================================================================
+*                                    SOURCE FILE VERSION INFORMATION
+==================================================================================================*/
+#define TRGMUX_IP_PBCFG_VENDOR_ID_C                        43
+#define TRGMUX_IP_PBCFG_AR_RELEASE_MAJOR_VERSION_C         4
+#define TRGMUX_IP_PBCFG_AR_RELEASE_MINOR_VERSION_C         7
+#define TRGMUX_IP_PBCFG_AR_RELEASE_REVISION_VERSION_C      0
+#define TRGMUX_IP_PBCFG_SW_MAJOR_VERSION_C                 2
+#define TRGMUX_IP_PBCFG_SW_MINOR_VERSION_C                 0
+#define TRGMUX_IP_PBCFG_SW_PATCH_VERSION_C                 0
+
+/*==================================================================================================
+*                                     FILE VERSION CHECKS
+==================================================================================================*/
+/* Check if Trgmux_Ip_PBcfg.c file and Trgmux_Ip_Cfg.h file are of the same vendor */
+#if (TRGMUX_IP_PBCFG_VENDOR_ID_C != TRGMUX_IP_CFG_VENDOR_ID)
+    #error "Trgmux_Ip_PBcfg.c and Trgmux_Ip_Cfg.h have different vendor ids"
+#endif
+
+/* Check if Trgmux_Ip_PBcfg.c file and Trgmux_Ip_Cfg.h file are of the same Autosar version */
+#if ((TRGMUX_IP_PBCFG_AR_RELEASE_MAJOR_VERSION_C != TRGMUX_IP_CFG_AR_RELEASE_MAJOR_VERSION) || \
+     (TRGMUX_IP_PBCFG_AR_RELEASE_MINOR_VERSION_C != TRGMUX_IP_CFG_AR_RELEASE_MINOR_VERSION) || \
+     (TRGMUX_IP_PBCFG_AR_RELEASE_REVISION_VERSION_C != TRGMUX_IP_CFG_AR_RELEASE_REVISION_VERSION) \
+    )
+    #error "AutoSar Version Numbers of Trgmux_Ip_PBcfg.c and Trgmux_Ip_Cfg.h are different"
+#endif
+
+/* Check if Trgmux_Ip_PBcfg.c file and Trgmux_Ip_Cfg.h file are of the same Software version */
+#if ((TRGMUX_IP_PBCFG_SW_MAJOR_VERSION_C != TRGMUX_IP_CFG_SW_MAJOR_VERSION) || \
+     (TRGMUX_IP_PBCFG_SW_MINOR_VERSION_C != TRGMUX_IP_CFG_SW_MINOR_VERSION) || \
+     (TRGMUX_IP_PBCFG_SW_PATCH_VERSION_C != TRGMUX_IP_CFG_SW_PATCH_VERSION) \
+    )
+    #error "Software Version Numbers of Trgmux_Ip_PBcfg.c and Trgmux_Ip_Cfg.h are different"
+#endif
+
+/*==================================================================================================
+*                                        LOCAL MACROS
+==================================================================================================*/
+
+/*==================================================================================================
+                          LOCAL TYPEDEFS (STRUCTURES, UNIONS, ENUMS)
+==================================================================================================*/
+
+/*==================================================================================================
+                                       LOCAL CONSTANTS
+==================================================================================================*/
+
+/*==================================================================================================
+                                       LOCAL VARIABLES
+==================================================================================================*/
+
+/*==================================================================================================
+                                       GLOBAL CONSTANTS
+==================================================================================================*/
+
+/*==================================================================================================
+                                       GLOBAL VARIABLES
+==================================================================================================*/
+
+/*==================================================================================================
+                                   LOCAL FUNCTION PROTOTYPES
+==================================================================================================*/
+
+/*==================================================================================================
+                                       LOCAL FUNCTIONS
+==================================================================================================*/
+
+/*==================================================================================================
+                                       GLOBAL FUNCTIONS
+==================================================================================================*/
+
+#ifdef __cplusplus
+}
+#endif
+
+/*==================================================================================================
+ *                                        END OF FILE
+==================================================================================================*/
---
+++ Documentation/NXP EB Tresos S32K144 MCAL drivers Project Examples/MemAcc_Example_S32K144/out/MemAcc_PBcfg.d
@@ -0,0 +1,35 @@
+MemAcc_PBcfg.o: generate/src/MemAcc_PBcfg.c \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/MemAcc_TS_T40D2M20I0R0/include/MemAcc_Types.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/StandardTypes.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Platform_Types.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/PlatformTypes.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Compiler.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Compiler_Cfg.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/CompilerDefinition.h \
+ generate/include/MemAcc_CfgDefines.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mem_43_INFLS_TS_T40D2M20I0R0/include/Mem_43_INFLS.h \
+ generate/include/Mem_43_INFLS_Cfg.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mem_43_INFLS_TS_T40D2M20I0R0/include/Mem_43_INFLS_Types.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mem_43_INFLS_TS_T40D2M20I0R0/include/Ftfc_Mem_InFls_Ip_Types.h \
+ generate/include/Mem_43_INFLS_CfgDefines.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Mem_43_INFLS_MemMap.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Rte_TS_T40D2M20I0R0/include/SchM_Mem_43_INFLS.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Rte_MemMap.h \
+ C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/MemAcc_MemMap.h
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/MemAcc_TS_T40D2M20I0R0/include/MemAcc_Types.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/StandardTypes.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Platform_Types.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/PlatformTypes.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Compiler.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Compiler_Cfg.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/CompilerDefinition.h:
+generate/include/MemAcc_CfgDefines.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mem_43_INFLS_TS_T40D2M20I0R0/include/Mem_43_INFLS.h:
+generate/include/Mem_43_INFLS_Cfg.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mem_43_INFLS_TS_T40D2M20I0R0/include/Mem_43_INFLS_Types.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Mem_43_INFLS_TS_T40D2M20I0R0/include/Ftfc_Mem_InFls_Ip_Types.h:
+generate/include/Mem_43_INFLS_CfgDefines.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Mem_43_INFLS_MemMap.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/Rte_TS_T40D2M20I0R0/include/SchM_Mem_43_INFLS.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/Rte_MemMap.h:
+C\:/NXP/SW32K1_S32M24x_RTD_R21-11_2.0.0_P04/eclipse/plugins/BaseNXP_TS_T40D2M20I0R0/include/MemAcc_MemMap.h:
